2f413edc25cae436ef880f028449ad0740a2cefe
LuisEscamilla9708 <127147479+LuisEscamilla9708@users.noreply.github.com>
2025-07-11 21:44:06 -0600

    funcion

---
+++ FizzBuzz.java
@@ -0,0 +1,19 @@
+package buzz
+
+public class FizzBuzz {
+
+    public void imprimir() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
